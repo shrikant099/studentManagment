42bb777e5c45d261eaeffe03070f18f2973e987b
Shrikant Soni <shrikantsoni809@gmail.com>
2025-01-20 15:44:51 +0530

    jh

---
+++ backend/main.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path'
+import { fileURLToPath } from 'url';
+const app = express();
+
+
+// dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get('/' , (req , res) => {
+    res.sendFile(path.join(__dirname , "../index.html"))
+});
+
+app.listen(9000 , (req , res) => {
+    console.log("App is listening on Port http://localhost:9000")
+});
